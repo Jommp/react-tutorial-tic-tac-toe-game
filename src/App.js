7dd1ddc7c3e5b508f9814f32aa43e138f0284c9e
Jommp <jommp.dev@gmail.com>
2025-06-24 23:47:15 -0500

    feat: add utilities folder and move handleWinner function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import { Square } from '../src/components';
+import { handleWinner } from './utils/utilities';
 
 export default () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
@@ -50,27 +51,4 @@ export default () => {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   </>;
-}
-
-const handleWinner = (squares) => {
-  const linesToWin = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < linesToWin.length; i++) {
-    const [a, b, c] = linesToWin[i];
-
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-
-  return null;
 }
